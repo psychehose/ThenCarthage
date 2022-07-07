7a89f30a811d5bacab184a1d95edecca113d74e6
psychehose <psychehose@gmail.com>
2022-07-07 13:14:09 +0900

    init

---
+++ Then/Then.h
@@ -0,0 +1,18 @@
+//
+//  Then.h
+//  Then
+//
+//  Created by KIM HOSE on 2022/07/07.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Then.
+FOUNDATION_EXPORT double ThenVersionNumber;
+
+//! Project version string for Then.
+FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Then/PublicHeader.h>
+
+
